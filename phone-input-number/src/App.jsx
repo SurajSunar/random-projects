05bb92be8daace9ba373b26ee9a97611e39d34b5
Suraj Sunar <surbhu06@gmail.com>
2025-09-30 21:40:40 +0600

    app: adding field mobile and phone

--- phone-input-number/src/App.jsx
+++ phone-input-number/src/App.jsx
@@ -1,32 +1,66 @@
 import { AutoComplete, Button, Form, Input } from "antd";
 import { Search } from "lucide-react";
+import { useState } from "react";
+import CountryList from "country-list-with-dial-code-and-flag";
 
-const Countries = [{
-  id: '+975',
-  name: 'Bhutan'
-}]
+const countries = CountryList.getAll().map((row) => ({
+  value: row.dialCode,
+  label: row.name,
+}));
 
 const App = () => {
+  const [form] = Form.useForm();
+  const [options, setOptions] = useState([]);
 
+  const searchCountry = (value) => {
+    return !value
+      ? [...countries]
+      : [...countries].filter((country) =>
+          country.label.toLowerCase().includes(value.toLowerCase())
+        );
+  };
+
+  const submitForm = () => {
+    console.log(form.getFieldValue());
+  };
+
+  const AutoCompleteField = ({ field }) => (
+    <Form.Item name={field}>
+      <AutoComplete
+        name={field}
+        options={options}
+        onSearch={(search) => setOptions(() => searchCountry(search))}
+        className="[&_div]:!rounded-none [&_div]:!rounded-l-lg !w-[100px]"
+        suffixIcon={<Search className="w-4" />}
+      />
+    </Form.Item>
+  );
 
   return (
     <div className="bg-gray-100 min-h-screen">
-      <div className=" mx-auto w-1/2 flex items-center h-screen">
-        <div className="w-full h-[200px] bg-gray-200 rounded-xl p-6">
-          <Form layout="vertical">
-            <Form.Item label="Mobile Number" name="mobile">
-              <div className="flex">
-              <AutoComplete
-               onSelect={value => alert(value)}
-               options={[{value: 'cat', label: 'Cat'}]}
-               filterOption = {(value, option) => alert(value, option)}
-               searchValue=""
-              className="!w-[10%] [&_div]:!rounded-none [&_div]:!rounded-l-lg" 
-              suffixIcon={<Search className="w-4"/>} 
-              style={{ width: 200 }}  />
-              <Input className="!w-[30%] !rounded-none !rounded-r-lg"></Input>
-              </div>
-              
+      <div className=" mx-auto lg:w-1/2 flex items-center h-screen">
+        <div className="w-full h-[300px] bg-gray-200 rounded-xl p-10">
+          <Form layout="vertical" onFinish={submitForm} form={form}>
+            <Form.Item
+              label="Mobile Number"
+              name="mobile"
+              rules={[{ required: true, message: "Please input your number!" }]}
+            >
+              <Input
+                className="lg:!w-[70%] !rounded-none !rounded-r-lg [&_.ant-form-item]:!mb-0 [&_input]:!p-2"
+                addonBefore={<AutoCompleteField field={"mobileCode"} />}
+              ></Input>
+            </Form.Item>
+
+            <Form.Item
+              label="Phone Number"
+              name="phone"
+              rules={[{ required: true, message: "Please input your number!" }]}
+            >
+              <Input
+                className="lg:!w-[70%] !rounded-none !rounded-r-lg [&_.ant-form-item]:!mb-0 [&_input]:!p-2"
+                addonBefore={<AutoCompleteField field={"phoneCode"} />}
+              ></Input>
             </Form.Item>
 
             <Form.Item label={null}>
