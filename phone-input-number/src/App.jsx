4ddcf7962e34b82102ce347e6a9de318a1116feb
Suraj Sunar <surbhu06@gmail.com>
2025-09-30 20:01:58 +0600

    app: layout of component with form submission

--- phone-input-number/src/App.jsx
+++ phone-input-number/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { AutoComplete, Button, Form, Input } from "antd";
+import { Search } from "lucide-react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Countries = [{
+  id: '+975',
+  name: 'Bhutan'
+}]
+
+const App = () => {
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="bg-gray-100 min-h-screen">
+      <div className=" mx-auto w-1/2 flex items-center h-screen">
+        <div className="w-full h-[200px] bg-gray-200 rounded-xl p-6">
+          <Form layout="vertical">
+            <Form.Item label="Mobile Number" name="mobile">
+              <div className="flex">
+              <AutoComplete
+               onSelect={value => alert(value)}
+               options={[{value: 'cat', label: 'Cat'}]}
+               filterOption = {(value, option) => alert(value, option)}
+               searchValue=""
+              className="!w-[10%] [&_div]:!rounded-none [&_div]:!rounded-l-lg" 
+              suffixIcon={<Search className="w-4"/>} 
+              style={{ width: 200 }}  />
+              <Input className="!w-[30%] !rounded-none !rounded-r-lg"></Input>
+              </div>
+              
+            </Form.Item>
 
-export default App
+            <Form.Item label={null}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
